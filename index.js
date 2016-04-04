dbf19261e88361c9a1ac6d202f750397a0324ca4
Aaron Robb <airrobb@gmail.com>
2016-04-04 19:22:12 -0400

    bad sort

--- index.js
+++ index.js
@@ -12,6 +12,20 @@ request
     }
     const stripped = R.replace(/(<script(\s|\S)*?<\/script>)|(<style(\s|\S)*?<\/style>)|(<!--(\s|\S)*?-->)|(<\/?(\s|\S)*?>)/g,'', data.text)
     const sentences = R.split('.', stripped)
-    const noBreaks = R.map(R.replace(/(?:\r\n|\r|\n|\t)/g, ''), sentences)
-    console.log(noBreaks)
+    const noBreaks = R.map(R.replace(/(?:\r\n|\r|\n|\t)|\[edit\]/g, ''), sentences)
+    const years = R.filter(findYears, noBreaks)
+    const sortedYears = R.sort(sortByYear, years)
+    console.log(sortedYears)
   })
+
+
+function findYears(string) {
+  const foundYears = R.match(/\d{4}|\d{3}/, string)
+  return foundYears.length > 0
+}
+
+function sortByYear(a, b) {
+  const yearA = R.match(/\d{4}|\d{3}/, a)
+  const yearB = R.match(/\d{4}|\d{3}/, b)
+  return yearA[0] - yearB[0]
+}
